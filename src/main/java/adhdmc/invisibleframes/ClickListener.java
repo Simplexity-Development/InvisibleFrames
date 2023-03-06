ae90dbb97680c38d70f19fc5b230bb4932da3d67
Peashooter101 <Peashooter101@yahoo.com>
2023-03-05 17:59:41 -0800

    v1.1 - ItemsAdder Support
    Added a check for ItemsAdder, allows for CustomStack that exists from ItemsAdder to be untouchable by this plugin.
    
    This preserves ItemsAdder's existing functionality with custom blocks / items that are placed into the world using invisible item frames.

--- src/main/java/adhdmc/invisibleframes/ClickListener.java
+++ src/main/java/adhdmc/invisibleframes/ClickListener.java
@@ -1,5 +1,7 @@
 package adhdmc.invisibleframes;
 
+import dev.lone.itemsadder.api.CustomStack;
+import dev.lone.itemsadder.api.ItemsAdder;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
@@ -24,6 +26,7 @@ public class  ClickListener implements Listener {
         Entity entity = event.getRightClicked();
         if (!(entity instanceof ItemFrame itemFrame)) return;
         if (itemFrame.getItem().getType().equals(Material.AIR) && !config.getBoolean("toggle-empty", false)) return;
+        if (InvisibleFrames.hasItemsAdder() && CustomStack.byItemStack(itemFrame.getItem()) != null) return;
         if (!player.hasPermission("invisibleframes.toggleframes")) return;
 
         event.setCancelled(true);
--- src/main/java/adhdmc/invisibleframes/InvisibleFrames.java
+++ src/main/java/adhdmc/invisibleframes/InvisibleFrames.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class InvisibleFrames extends JavaPlugin {
 
     private static InvisibleFrames instance;
+    private static boolean hasItemsAdder;
 
     @Override
     public void onEnable() {
@@ -14,9 +15,17 @@ public final class InvisibleFrames extends JavaPlugin {
         getConfig().addDefault("lock-frame", true);
         getConfig().addDefault("toggle-empty", false);
         instance = this;
+        hasItemsAdder = false;
+        try {
+            Class.forName("dev.lone.itemsadder.api.CustomBlock");
+            hasItemsAdder = true;
+        } catch (ClassNotFoundException e) {
+            this.getLogger().info("ItemsAdder API has not been found, ItemsAdder checks are disabled.");
+        }
     }
 
     public static InvisibleFrames getInstance() {
         return instance;
     }
+    public static boolean hasItemsAdder() { return hasItemsAdder; }
 }
